c259013215ca2cf49139c80325003a0cb57a04d5
Piotr Mas³owski <p.maslowski94@gmail.com>
2015-11-23 17:42:21 +0100

    dodano pomiar czasu

--- Weather.php
+++ Weather.php
@@ -1,12 +1,14 @@
 <?php
 
 include_once 'vendor/autoload.php';
+
 use Pmas\Component\Weather\Api\OpenWeatherClient;
 
 
 $browser = new Buzz\Browser();
+$stoper = new Stopwatch();
 echo "Podaj nazwe miasta: ";
 $handle = fopen ("php://stdin","r");
 $line = fgets($handle);
 
-$city = new OpenWeatherClient($line, $browser);
+$city = new OpenWeatherClient($line, $browser, $stoper);
--- src/Pmas/Component/Weather/Api/OpenWeatherClient.php
+++ src/Pmas/Component/Weather/Api/OpenWeatherClient.php
@@ -8,23 +8,30 @@ class OpenWeatherClient
 
 	private $line;
 	private $browser;
-	
-	public function __construct($line, $browser) {
+	private $stoper	
+
+	public function __construct($line, $browser, $stoper) {
 		
 		$this->browser = $browser;
 		$this->line = $line;
+		$this->stoper = $stoper;
 		$url = 'http://api.openweathermap.org/data/2.5/weather?q=' . $line . '&APPID=96cdeb166e66f9c035e9e7f8ce665ec8';
 		$line[strlen($line)-1] = NULL;
 
 		$response = $browser->get($url);
 		$content = $response->getContent();
+		$stoper->start('test');
 		$weatherObject = json_decode($content);
-	
+		
+		$czas = $stoper->stop('test');
 		echo 'Temperatura w ' . "$line" . ' wynosi ';
 		$temp = $weatherObject->main->temp-273;
 		echo "$temp";
 		echo " stopni Celcjusza";
 		echo "\n";
+		$czas->getDuration();
+		echo "Czas pobierania: ";
+		echo $czas;	
 
 
 
