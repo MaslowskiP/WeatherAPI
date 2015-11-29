5f60847183f2a7bd05cf16f74b5389d96746b65a
Piotr Mas³owski <p.maslowski94@gmail.com>
2015-11-29 18:10:43 +0100

    dodano zapisywanie temperatury i pobieranie jej z pliku jezeli ostatni pomiar byl przeprowadzony mniej niz 10 min temu

--- Weather.php
+++ Weather.php
@@ -4,11 +4,14 @@ include_once 'vendor/autoload.php';
 
 use Pmas\Component\Weather\Api\OpenWeatherClient;
 use Symfony\Component\Stopwatch\Stopwatch;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\Filesystem\Exception\IOExceptionInterface;
 
+$fs = new Filesystem();
 $browser = new Buzz\Browser();
 $stoper = new Stopwatch();
 echo "Podaj nazwe miasta: ";
 $handle = fopen ("php://stdin","r");
 $line = fgets($handle);
 
-$city = new OpenWeatherClient($line, $browser, $stoper);
+$city = new OpenWeatherClient($line, $browser, $stoper, $fs);
--- src/Pmas/Component/Weather/Api/OpenWeatherClient.php
+++ src/Pmas/Component/Weather/Api/OpenWeatherClient.php
@@ -8,36 +8,44 @@ class OpenWeatherClient
 
 	private $line;
 	private $browser;
-	private $stoper;	
+	private $stoper;
+	private $fs;	
 
-	public function __construct($line, $browser, $stoper) {
+	public function __construct($line, $browser, $stoper, $fs) {
 		
 		$this->browser = $browser;
 		$this->line = $line;
 		$this->stoper = $stoper;
+		$this->fs = $fs;
 		$stoper->start('test');
 		$url = 'http://api.openweathermap.org/data/2.5/weather?q=' . $line . '&APPID=96cdeb166e66f9c035e9e7f8ce665ec8';
-		$line[strlen($line)-1] = NULL;
-
-		$response = $browser->get($url);
-		$content = $response->getContent();
+		$line = trim($line);
+		$sprawdzaniePliku = '/home/studenci/r12/s181008/public_html/web/pp16/' . $line . '.tmp';
 		
-		$weatherObject = json_decode($content);
+    		if (file_exists($sprawdzaniePliku) && filemtime($sprawdzaniePliku) > time()-600) {
+			$czas = $stoper->stop('test');
+			$file=fopen($sprawdzaniePliku, "r");  
+			flock($file, 1);  
+			$src=fgets($file, 30);
+			fclose($file); 
+			echo 'Temperatura w ' . "$line" . ' wynosi ' . $src . " stopni Celcjusza.\n";
+ 			echo 'Dane z: '. date ("d m Y H:i:s.", filemtime($sprawdzaniePliku));
+		} else {
+			$response = $browser->get($url);
+			$content = $response->getContent();
 		
-		$czas = $stoper->stop('test');
-		echo 'Temperatura w ' . "$line" . ' wynosi ';
-		$temp = $weatherObject->main->temp-273.15;
-		$temp = round($temp);
-		echo "$temp";
-		echo " stopni Celcjusza\n";
-		echo "Czas pobierania: ";
-		$pomiar = var_export(round($czas->getDuration()/1000));
-		echo $pomiar . " sekund.\n";	
-
-
-
-
+			$weatherObject = json_decode($content);
+		
+			$czas = $stoper->stop('test');
+			echo 'Temperatura w ' . "$line" . ' wynosi ';
+			$temp = $weatherObject->main->temp-273.15;
+			$temp = round($temp);
+			echo "$temp";
+			echo " stopni Celcjusza\n";
+			echo "Czas pobierania: ";
+			$pomiar = var_export(round($czas->getDuration()/1000));
+			echo $pomiar . " sekund.\n";
+			$fs->dumpFile($sprawdzaniePliku , $temp);	
+		}
 	}
-
-
 }
